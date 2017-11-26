852f64a4391de66e3ae376bb1c0c1fe3ffc05a97
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-11-26 01:14:10 +0100

    removing twitch routes

--- routes.js
+++ routes.js
@@ -9,18 +9,6 @@ module.exports = function(app) {
 
   app.use( '/api/v3', require('./routes/api/v3/index') );
   
-  app.use( '/api/v3/twitch', require('./routes/api/v3/twitch/index') );
-  app.use( '/api/v3/twitch/hosts', require('./routes/api/v3/twitch/hosts/index') );
-  app.use( '/api/v3/twitch/viewers', require('./routes/api/v3/twitch/viewers/index') );
-  app.use( '/api/v3/twitch/latestfollower', require('./routes/api/v3/twitch/latestfollower/index') );
-  app.use( '/api/v3/twitch/followercount', require('./routes/api/v3/twitch/followercount/index') );
-  app.use( '/api/v3/twitch/streamstatus', require('./routes/api/v3/twitch/streamstatus/index') );
-
-  app.use( '/api/v3/twitch/utils', require('./routes/api/v3/twitch/utils/index') );
-  app.use( '/api/v3/twitch/utils/getId', require('./routes/api/v3/twitch/utils/getId/index') );
-  app.use( '/api/v3/twitch/utils/getUsername', require('./routes/api/v3/twitch/utils/getUsername/index') );
-  app.use( '/api/v3/twitch/utils/getLogo', require('./routes/api/v3/twitch/utils/getLogo/index') );
-
   app.use( '/api/v3/sc2', require('./routes/api/v3/sc2/index') );
   app.use( '/api/v3/sc2/bnet', require('./routes/api/v3/sc2/bnet/index') );
 
