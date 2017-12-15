d4ebb5481cd50f25d9b6984024fe78bb324e8b94
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-15 21:42:52 +0100

    moving routes.js back to root folder

---
+++ routes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(app) {
+
+  app.use( '/', require('./routes/index') );
+
+  // v1
+
+  app.use( '/v1/', require('./routes/v1/index') );
+  
+  app.use( '/v1/bnet', require('./routes/v1/bnet/index') );
+  
+  // app.use( '/v1/sc2', require('./routes/v1/sc2/index') );
+  // app.use( '/v1/sc2/profile', require('./routes/v1/sc2/profile/index') );
+  // app.use( '/v1/sc2/ladders', require('./routes/v1/sc2/ladders/index') );
+  // app.use( '/v1/sc2/mmr', require('./routes/v1/sc2/mmr/index') );
+
+};
