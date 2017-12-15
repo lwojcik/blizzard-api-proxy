c86dc2daeee0880a00b529be741f5f08ce6cc831
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-15 22:10:11 +0100

    fixed import paths

--- routes.js
+++ routes.js
@@ -10,9 +10,9 @@ module.exports = function(app) {
   
   app.use( '/v1/bnet', require('./routes/v1/bnet/index') );
   
-  // app.use( '/v1/sc2', require('./routes/v1/sc2/index') );
-  // app.use( '/v1/sc2/profile', require('./routes/v1/sc2/profile/index') );
-  // app.use( '/v1/sc2/ladders', require('./routes/v1/sc2/ladders/index') );
-  // app.use( '/v1/sc2/mmr', require('./routes/v1/sc2/mmr/index') );
+  app.use( '/v1/sc2', require('./routes/v1/sc2/index') );
+  app.use( '/v1/sc2/profile', require('./routes/v1/sc2/profile/index') );
+  app.use( '/v1/sc2/ladders', require('./routes/v1/sc2/ladders/index') );
+  app.use( '/v1/sc2/mmr', require('./routes/v1/sc2/mmr/index') );
 
 };
