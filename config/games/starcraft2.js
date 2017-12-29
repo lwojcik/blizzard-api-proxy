19af61f50b010e789c922fbda60032dc4d2369ca
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-29 21:01:53 +0100

    adding config parameter for archon ladder

--- config/games/starcraft2.js
+++ config/games/starcraft2.js
@@ -16,6 +16,6 @@ sc2Config.matchMaking = {};
 /** StarCraft 2 matchmaking modes as used in Battle.net API */
 sc2Config.matchMaking.modes = ['1V1', '2V2', '3V3', '4V4'];
 /** StarCraft 2 matchmaking queues as used in Battle.net API */
-sc2Config.matchMaking.queues = ['LOTV_SOLO', 'LOTV_TWOS', 'LOTV_THREES', 'LOTV_FOURS'];
+sc2Config.matchMaking.queues = ['LOTV_SOLO', 'LOTV_TWOS', 'LOTV_TWOS_COMP', 'LOTV_THREES', 'LOTV_FOURS'];
 
 module.exports = sc2Config;
